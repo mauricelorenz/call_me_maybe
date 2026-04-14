fdfb88da4025be94a34fdf9788f8d3129ac6a70b
Maurice Lorenz <mlorenz@student.42heilbronn.de>
2026-04-14 15:28:07 +0200

    Add parse_infile()

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,17 @@
 import argparse
+import json
+from sys import exit
+from typing import List, Dict
+
+
+def parse_infile(path: str) -> List[Dict[str, str]]:
+    try:
+        with open(path) as f:
+            infile: List[Dict[str, str]] = json.loads(f.read())
+    except (FileNotFoundError, PermissionError, json.JSONDecodeError) as e:
+        print(f"Error while parsing {path}: {e}")
+        exit(1)
+    return infile
 
 
 def main() -> None:
@@ -13,7 +26,10 @@ def main() -> None:
     parser.add_argument("--output",
                         default="data/output/function_calls.json",
                         help="path for the output json file")
-    # args = parser.parse_args()
+    args = parser.parse_args()
+    functions_definition = parse_infile(args.functions_definition)
+    input = parse_infile(args.input)
+    print(functions_definition, "\n\n", input)
 
 
 if __name__ == "__main__":
