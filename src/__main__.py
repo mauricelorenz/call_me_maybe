8568a0272831619b7f29ab9f2b2fc4022922d397
Maurice Lorenz <me@mauricelorenz.com>
2026-04-21 00:51:49 +0200

    Implement call_llm() and generate_outfile()

--- src/__main__.py
+++ src/__main__.py
@@ -1,46 +1,50 @@
 import argparse
 import json
 from sys import exit
-from typing import List, Dict
+from typing import List, Dict, Any
 import llm_sdk
 import numpy as np
 
 
-def test_llm(fd: List[Dict[str, str]], input: List[Dict[str, str]]) -> None:
-    llm = llm_sdk.Small_LLM_Model()  # type: ignore
-    fd_string = json.dumps(fd)
-    prompt = (f"Pick a function matching the question 'What is the sum of 2 a"
-              f"nd 3?' out of the following: {fd_string} and return the name")
-    tokens = llm.encode(prompt)
-    # decoded = llm.decode(tokens)
+def call_llm(llm: Any, full_prompt: str) -> str:
+    tokens = llm.encode(full_prompt)
     tokens_list = tokens[0].tolist()
-    # print(parse_infile(llm.get_path_to_tokenizer_file()))
-    i = 50
+    initial_tokens_len = len(tokens_list)
+    i = 100
     while i:
         logits = llm.get_logits_from_input_ids(tokens_list)
         next_token = np.argmax(logits)
         tokens_list.append(next_token)
+        try:
+            current_answer = llm.decode(tokens_list[initial_tokens_len - 1:])
+            json.loads(current_answer)
+            break
+        except Exception:
+            pass
         i -= 1
-    print(f"Tokens: {tokens_list}\n")
-    print(f"String: {llm.decode(tokens_list)}")
-    # vocab_file = llm.get_path_to_vocab_file()
-    # print(vocab_file)
-    # vocab_content = parse_infile(vocab_file)
-    # special_tokens = {}
-    # for key in vocab_content.keys():
-    #     if "endof" in key:
-    #         special_tokens[key] = vocab_content[key]
-    # print(special_tokens)
-    # print(f"Tokens: {tokens}")
-    # print(f"String: {decoded}")
-    # print(f"Logits: {logits}")
-    # print(tokens.shape)
+    result_tokens = tokens_list[initial_tokens_len - 1:]
+    result_string: str = llm.decode(result_tokens)
+    return result_string
+
+
+def generate_outfile(functions_definition: List[Dict[str, str]],
+                     input: List[Dict[str, str]]) -> None:
+    llm = llm_sdk.Small_LLM_Model()  # type: ignore
+    fd_string = json.dumps(functions_definition)
+    for prompt in input:
+        prompt_string = prompt["prompt"]
+        full_prompt = (f"Pick a function matching the question "
+                       f"'{prompt_string}' out of the following: "
+                       f"{fd_string} and return only a JSON containing "
+                       f"prompt, name and the parameters:\n{{")
+        result_string = call_llm(llm, full_prompt)
+        print(result_string)
 
 
 def parse_infile(path: str) -> List[Dict[str, str]]:
     try:
         with open(path) as f:
-            infile: List[Dict[str, str]] = json.loads(f.read())
+            infile: List[Dict[str, str]] = json.load(f)
     except (FileNotFoundError, PermissionError, json.JSONDecodeError) as e:
         print(f"Error while parsing {path}: {e}")
         exit(1)
@@ -52,17 +56,17 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--functions_definition",
                         default="data/input/functions_definition.json",
-                        help="path for the functions definition json file")
+                        help="path for the functions definition JSON file")
     parser.add_argument("--input",
                         default="data/input/function_calling_tests.json",
-                        help="path for the input json file")
+                        help="path for the input JSON file")
     parser.add_argument("--output",
                         default="data/output/function_calls.json",
-                        help="path for the output json file")
+                        help="path for the output JSON file")
     args = parser.parse_args()
     functions_definition = parse_infile(args.functions_definition)
     input = parse_infile(args.input)
-    test_llm(functions_definition, input)
+    generate_outfile(functions_definition, input)
 
 
 if __name__ == "__main__":
